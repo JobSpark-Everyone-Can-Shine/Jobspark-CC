cc6630199088bfe1da14f4441bd4b0ad307488b6
rangga <arsyrangga@gmail.com>
2024-11-28 13:05:50 +0700

    jobs

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { auth } = require("../utils/helper");
 const jobsController = require("../controllers/jobs");
 
-router.get("/", auth, jobsController.getJobs);
+router.get("/", jobsController.getJobs);
 router.get("/:id", auth, jobsController.getJobDetail);
 router.post("/apply", auth, jobsController.applyJob);
 
